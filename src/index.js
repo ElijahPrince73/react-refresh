58a76da489c7db6ed9a0f7fe2ebbd45cd40cb141
Elijah Prince <elijahprince73@gmail.com>
2023-09-25 22:34:54 -0700

    Exporting and import the FILES

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 // 1) import React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
- 
+import App from './App'
+
 // 2) Reference the div with ID ROOT
 const el = document.getElementById("root");
- 
+
 // 3) Tell react to take control of that element
 const root = ReactDOM.createRoot(el);
-// 4) Create a component
-function App() {
-  const inputType = "number";
-  const minValue = 5
 
-  return <textarea autoFocus/>
-}
-// 5) Show the component in the screen
+// 4) Show the component in the screen
 root.render(<App />);
