0d23067fc5c3495e7949d0262edb4c0ad60028ef
Elijah Prince <elijahprince73@gmail.com>
2023-10-03 16:41:21 -0700

    adding bulma css and some component creation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import ProfileCard from "./ProfileCard"
+
+import AlexaImg from './images/alexa.png'
+import CortanaImg from './images/cortana.png'
+import SiriImg from './images/siri.png'
+
 function App() {
-  return <h1>Hello</h1>
+  return (
+    <div>
+      <div>Personal Digital Assistants</div>
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg}/>
+      <ProfileCard title="Siri" handle="@siri " image={SiriImg}/>
+    </div>
+  );
 }
 
 export default App
