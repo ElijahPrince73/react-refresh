9a2ad6a25eb6b4cd121eae9fb06dbc62957f4f11
Elijah Prince <elijahprince73@gmail.com>
2023-10-03 16:52:15 -0700

    some styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'bulma/css/bulma.css'
+
 import ProfileCard from "./ProfileCard"
 
 import AlexaImg from './images/alexa.png'
@@ -8,9 +10,21 @@ function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg}/>
-      <ProfileCard title="Siri" handle="@siri " image={SiriImg}/>
+      <div className='container'>
+        <section>
+          <div className='columns'>
+            <div className='column is-4'>
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
+            </div>
+            <div className='column is-4'>
+              <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg}/>
+            </div>
+            <div className='column is-4'>
+              <ProfileCard title="Siri" handle="@siri " image={SiriImg}/>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,21 @@
 const ProfileCard = ({ title, handle, image }) => {
   
   return (
-    <div>
-      <img src={image} alt={title} />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt={title} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">Title is {title}</p>
+            <p className="subtitle is-6">Handle {handle}</p>
+          </div>
+        </div>
     </div>
+  </div>
   )
 }
 
